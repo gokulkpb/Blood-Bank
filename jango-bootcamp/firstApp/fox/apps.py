91739895fd222c68169960bafe841641162accee
Gokul Balachandran <gokulkpb@gmail.com>
2021-10-08 23:20:33 +0530

    Delete jango-bootcamp directory

--- jango-bootcamp/firstApp/fox/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ActivelloConfig(AppConfig):
-    name = 'activello'
--- jango-bootcamp/firstApp/fox/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Entry:
-    def __init__(self, head, desc):
-        self.head = head
-        self.desc = desc
